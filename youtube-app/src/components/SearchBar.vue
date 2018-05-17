9df3300029998072ceb2d053afef9664c8255618
Daniel Ward <danward@gmail.com>
2018-05-17 14:01:53 -0400

    Add App and SearchBar components

---
+++ youtube-app/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input v-on:input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput: (event) => {
+      console.log(event.target.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
