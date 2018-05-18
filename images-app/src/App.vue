09b7cb7eeabf936eb11992d8a14b571e23611c0b
Daniel Ward <danward@gmail.com>
2018-05-18 11:37:46 -0400

    Add AppHeader, SemanticUI

--- images-app/src/App.vue
+++ images-app/src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AppHeader />
+    <h3>App!</h3>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ images-app/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="/"
+    class="active item">
+      Imagerie
+    </a>
+    <div class="right menu">
+      <a class="item">
+        Logout
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
